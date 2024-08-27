89b7eb3f49daf498c0ad7881dd6680c199b1fd29
AwakeningSoul <weathergoals@gmail.com>
2024-08-27 13:09:29 +0100

    Add project files.

---
+++ AbstractClasses/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClasses
+{
+    //Create an abstract class called Person with two properties: string firstName and string lastName.
+    public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        //Give it the method SayName()
+        public abstract void SayName();
+    }
+}
---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AbstractClasses
+{
+       class Program
+        {
+        static void Main(string[] args)
+        {
+            //Inside the Main() method, instantiate an Employee object 
+            Employee sam = new Employee() { firstName = "Sample", lastName = " Student" };
+            //Call the SayName method on the object
+            sam.SayName();
+            Console.ReadLine();
+        }
+    }   
+}
+
